525da07424c7a6cb25ec6e3fd00635cafdda02fc
xinyi <xinyi2017mm@gmail.com>
2019-01-27 12:56:17 -0800

    add

--- libft.h
+++ libft.h
@@ -30,10 +30,17 @@ char	*ft_strncpy(char *dst, const char *src, size_t len);
 char    *ft_strcat(char *dest, const char *src);
 char    *ft_strncat(char *s1, const char *s2, size_t n);
 
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strstr(const char *haystack, const char *needle);
+
 int ft_strcmp(const char *s1, const char *s2);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 
+char	*ft_strdup(const char *s1);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+char    *ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
 size_t	ft_strlen(const char*str);
 
