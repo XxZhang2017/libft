3c05d706ac62be71bd156430e2e1d71cc5814489
xinyi <xinyi2017mm@gmail.com>
2019-01-27 12:53:01 -0800

    add

---
+++ ft_strncat.c
@@ -0,0 +1,8 @@
+
+
+#include "libft.h"
+
+char    *ft_strncat(char *s1, const char *s2, size_t n)
+{
+    return (s1);
+}
--- libft.h
+++ libft.h
@@ -26,6 +26,15 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 char    *ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
+
+char    *ft_strcat(char *dest, const char *src);
+char    *ft_strncat(char *s1, const char *s2, size_t n);
+
+int ft_strcmp(const char *s1, const char *s2);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+
 size_t	ft_strlen(const char*str);
 
 int ft_atoi(const char *str);
