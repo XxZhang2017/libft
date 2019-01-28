30d8d648d8a3e148d24200663bafe36344c83a99
xinyi <xinyi2017mm@gmail.com>
2019-01-27 20:00:21 -0800

    add

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -8,12 +8,12 @@ int ft_strcmp(const char *s1, const char *s2)
 {
     int i;
 
-    i = 0;
+    i = -1;
     if (!s1)
         return ((unsigned char)s2);
     if (!s2)
         return ((unsigned char)s1); 
-    while (i++ && s1[i] && s2[i] && s1[i] == s2[i])
+    while (s1[++i] && s2[i] && s1[i] == s2[i])
         ;
-    return ((unsigned char)s1 - (unsigned char)s2);
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
