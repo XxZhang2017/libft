e60fbdddda81bcb75b6fbca2c8cdd563a82f7b6d
XxZhang2017 <mynewvegaan@gmail.com>
2019-01-28 16:01:09 -0800

    add

--- ft_strncat.c
+++ ft_strncat.c
@@ -4,5 +4,15 @@
 
 char    *ft_strncat(char *s1, const char *s2, size_t n)
 {
+    ft_strncpy(s1 + ft_strlen(s1), s2, n);
     return (s1);
+}
+
+int main()
+{
+    char    ch[100] = "i like the cat";
+    char    *append = "   Really?";
+
+    printf("%s\n", ft_strncat(ch, append, 5));
+    return (0);
 }
