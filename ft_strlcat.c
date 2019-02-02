8ef84130365dee893a25f7558a25fc451917f7cf
xinyi <xinyi2017mm@gmail.com>
2019-02-01 16:21:42 -0800

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,6 +5,7 @@
 
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+
     ft_strncat(dst + ft_strlen(dst), src, dstsize);
-    return (dstsize);
+    return (dstsize +ft_strlen(dst));
 }
