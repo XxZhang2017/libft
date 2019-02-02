d822e579a6a4ab8e59c576f011b74c8d828ccdb9
xinyi <xinyi2017mm@gmail.com>
2019-02-01 17:01:52 -0800

    Add

--- ft_strchr.c
+++ ft_strchr.c
@@ -16,13 +16,13 @@ char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
 		return (NULL);
-	if (c == '\0')
-		return ((char *)(s + ft_strlen(s) - 1));
 	while (*s)
 	{
 		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
+	if (c == '\0')
+		return ((char *)(s + ft_strlen(s) - 1));
 	return (NULL);
 }
