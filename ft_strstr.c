d14d89003a735d218b983987de33e87e97556e37
xinyi <xinyi2017mm@gmail.com>
2019-02-01 22:41:13 -0800

    add

--- ft_strstr.c
+++ ft_strstr.c
@@ -20,16 +20,17 @@ char	*ft_strstr(const char *haystack, const char *needle)
 
 	i = 0;
 	j = 0;
-	if (!haystack)
+	if (!ft_strlen(haystack))
 		return  (NULL);
 	if (!needle)
 		return ((char *)&haystack[ft_strlen(haystack) - 1]);
+	if (needle && !ft_strlen(needle))
+		return ((char *)haystack);
 	while (haystack[i] && needle[j])
 	{	
 		j = 0;
 		if (haystack[i] == needle[j])
-		{
-			
+		{		
 			temp = i;	
 			while (needle[j] && (haystack[i] == needle[j]))
 			{
--- main.c
+++ main.c
@@ -2,15 +2,14 @@
 //#include "libft.h"
 #include "libft.h"
 int main() 
-{ 
-char	*n = "945";
+{
+	char	*s1 = "oh no not the empty string !";
+			char	*s2 = "";
 
-			int		i1 = atoi(n);
-			int		i2 = ft_atoi(n);
-			printf("atoi return %d\n", i1);
-			printf("ft_atoi return %d\n", i2);
-			if (i1 == i2)
-				printf("success\n");
-
-			return (0);
+			char	*i1 = strstr(s1, s2);
+			char	*i2 = ft_strstr(s1, s2);
+	printf("s1 is %s\n", s1);
+	printf("s2 is %s\n", s2);
+	if (i1 == i2)
+		printf("success\n");
 } 
