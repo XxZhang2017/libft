614283847b9f9f99644fc64b156122cdfc597375
xinyi <xinyi2017mm@gmail.com>
2019-02-02 16:53:28 -0800

    add

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,19 +10,8 @@ char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	j = 0;
-<<<<<<< HEAD
 	if (!haystack || !needle)
 		return  (NULL);
-=======
-	// if (!haystack)
-	// 	return  (NULL);
-	// if (!needle)
-	// 	return ((char *)&haystack[ft_strlen(haystack) - 1]);
-	if (!ft_strlen(haystack))
-		return  (NULL);
-	if (!needle)
-		return ((char *)&haystack[ft_strlen(haystack) - 1]);
->>>>>>> b8bfb8f5f89aea76980c2e53468969d2f6bf104f
 	if (needle && !ft_strlen(needle))
 		return ((char *)haystack);
 	while (i < (int)len && haystack[i] && needle[j])
