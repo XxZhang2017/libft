636f6d493d31dcb1f2f3ce0a0546a0bf925d8b25
XxZhang2017 <mynewvegaan@gmail.com>
2019-02-05 10:48:49 -0800

    fix norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,53 +6,15 @@
 /*   By: xinzhang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 10:39:13 by xinzhang          #+#    #+#             */
-/*   Updated: 2019/02/04 10:39:15 by xinzhang         ###   ########.fr       */
+/*   Updated: 2019/02/05 10:45:14 by xinzhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// int     ft_isspace(char a)
-// {
-//     if (a == ' ' || a == '\f' || a == '\t' || a == '\v')
-//         return (1);
-//     return (0);
-// }
-
-// char    *ft_strtrim(char const *s)
-// {
-//     char *reg;
-//     int i;
-//     int j;
-
-//     j = ft_strlen(s) - 1;
-//     if (!s)
-//         return (NULL);
-//     reg = ft_strnew(ft_strlen(s));
-//     i = 0;
-//     j = 0;
-//     while (s[i])
-//     {
-//         if (!ft_isspace(s[i]))
-//             break;
-//         i++;
-//     }
-//     while (j > 0)
-//     {
-//         if (!ft_isspace(s[j]))
-//             break;
-//         j--;
-//     }
-//     if (i < j && s[i])
-//         reg = ft_strncpy(reg, &s[i], j - i + 1);
-//     else
-//         reg = "";
-//     return (reg);
-// }
-
 char	*ft_strtrim(char const *s)
 {
-	char const *s_end;
+	char const	*s_end;
 
 	if (s == NULL)
 		return (NULL);
