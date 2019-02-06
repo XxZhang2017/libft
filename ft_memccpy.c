467092a5553fe46020a67d9aef0b98bb661243bb
XxZhang2017 <mynewvegaan@gmail.com>
2019-02-05 22:30:37 -0800

    add

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,17 +6,17 @@
 /*   By: xinzhang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 10:45:15 by xinzhang          #+#    #+#             */
-/*   Updated: 2019/02/04 00:29:09 by xinzhang         ###   ########.fr       */
+/*   Updated: 2019/02/05 22:30:19 by xinzhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void				*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
-	size_t	i;
+	size_t			i;
 
 	i = 0;
 	ptr1 = (unsigned char *)dst;
